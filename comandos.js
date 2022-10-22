eae743a3b862717ade584d0ed80255ccf8b1bf64
CarlaS7 <suarezcaarla@gmail.com>
2022-10-22 20:59:44 -0300

    agregamos comandos

---
+++ comandos.js
@@ -0,0 +1,16 @@
+/*
+
+1=>Para añadir todos los cambios
+git add .
+
+2=>Para confirmar los cambios
+git commit -m "texto descriptivo"
+
+3=>Para elevar al repositorio remoto en GIT HUB
+git push
+
+*Para saber el estado de los archivos
+git status
+(Este comando podemos hacerlo varias veces y en cualquier momento)
+
+*/
